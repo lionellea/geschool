2712ed7d9134ffc81b8cfa3a6ee8ce21c3593b68
The Pocket <temaniigor@gmail.com>
2019-08-10 18:54:22 +0100

    Pansion sans recu

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Eleve;
 use App\Entity\Tranche;
 use App\Entity\Inscription;
+use App\Entity\Pansion;
 use App\Repository\EleveRepository;
 use App\Repository\TrancheRepository;
 use App\Repository\InscriptionRepository;
+use App\Repository\PansionRepository;
 use App\Repository\SalleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -105,6 +107,59 @@ class DefaultController extends AbstractController
 
         }
 
+    }
+
+    /**
+     * @Route("/tranche/{id}", name="pay_tranche", methods={"GET","POST"})
+     */
+    public function tranche(Request $request, EleveRepository $eleveRepository, $id, PansionRepository $pansionRepository,TrancheRepository $trancheRepository): Response
+    {
+        if($eleve = $eleveRepository->findOneById($id)){
+            $salle = $eleve->getSalle();
+            $classe = $salle->getClasse();
+            $pansion = $pansionRepository->findOneBy(['eleve' => $eleve, 'salle' => $salle], ['id' => 'DESC']);
+            $tranches = null;
+
+            if($pansion)
+                $tranches = $trancheRepository->findBy(['pansions' => $pansion]);
+
+            if($request->getMethod() == 'GET' && ($request->get("montant") || $request->get("montantT"))){
+                $montant = intVal($request->get("montant"));
+                $id = $request->get("id");
+                if(! $pansion){
+                    $em = $this->getDoctrine()->getManager();
+                    $montantT = $request->get("montantT");
+
+                    $pansion = new Pansion();
+                    $pansion->setMontant($montantT)
+                        ->setEleve($eleve)
+                        ->setSalle($salle)
+                        ->setDatePaiement(new \DateTime('now'));
+
+                    $em->persist($pansion);
+                    $em->flush($pansion);
+                }
+
+                $em = $this->getDoctrine()->getManager();
+                
+                $tranche = new Tranche();
+                $tranche->setCode($trancheRepository->genCode())
+                    ->setMontant($montant);
+
+                $em->persist($tranche);
+                $em->flush($tranche);
+            }
+
+            return $this->render('pay_tranche.html.twig', [
+                'eleve' => $eleve,
+                'salle' => $salle,
+                'classe' => $classe,
+                'pansion' => $pansion,
+                'tranches' => $tranches
+            ]);
+        }else{
+            return null;
+        }
     }
    
 }
